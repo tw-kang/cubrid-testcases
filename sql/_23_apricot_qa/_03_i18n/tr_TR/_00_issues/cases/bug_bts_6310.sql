ea6c41d8b3930fc0b68069a0416fd0dbcf7a6bd6
Song <joykevin@cubrid.com>
2020-07-28 09:34:52 +0900

    Modified the test cases.
    Modified list
    - _23_apricot_qa
    - _27_banana_qa
    - _30_banana_pie_qa

--- sql/_23_apricot_qa/_03_i18n/tr_TR/_00_issues/cases/bug_bts_6310.sql
+++ sql/_23_apricot_qa/_03_i18n/tr_TR/_00_issues/cases/bug_bts_6310.sql
@@ -5,17 +5,13 @@ create class t(
     col3      string,
     col4       varchar(40) 
 );
-insert into t value('2011-05-01 PAZAR 10:11:12.678','2011-05-01 PAZAR 10:11:12','10:11:12 teIt pm','2011-05-01 PAZAR');
+insert into t value('2011-05-01 PAZAR 10:11:12.678','2011-05-01 PAZAR 10:11:12','10:11:12 pm','2011-05-01 PAZAR');
 insert into t value('2011-5-1 22:23:24.678','2011-05-01 10:11:12','10:11:12 pm','2011-05-01');
 --test
 ALTER table t change col1 Çç_Ğğ DATETIME;
 --test
 ALTER table t change col2 İı_Öö Timestamp;
 --test
-ALTER table t change col3 Şş_Üü Time;
-select * from t order by 1;
---test
-UPDATE t set col3='10:11:12 pm' where col3='10:11:12 teIt pm';
 ALTER table t change col3 Şş_Üü Time;
 --test
 ALTER table t modify col4  DATE;
